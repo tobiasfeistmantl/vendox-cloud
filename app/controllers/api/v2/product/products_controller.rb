50e2d94da98e436fc0e69c8c846b9e2cfa002efd
Tobias Feistmantl <tobias@feistmantl.net>
2015-08-13 23:58:38 +0200

    Update to API V2 and remove clothes temporary from views

--- app/controllers/api/v2/product/products_controller.rb
+++ app/controllers/api/v2/product/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V2::Product::ProductsController < Api::V2::Product::Base
 	def index
-		@q = ::Product.active.includes(:company, clothing_sizes: [:colors]).ransack(params[:q])
+		@q = ::Product.active.includes(:company).ransack(params[:q])
 
 		if coordinates = @user_session.coordinates
 			@products = @q.result.near(coordinates, 5000).paginate(page: params[:page])
--- app/views/api/v2/product/products/index.json.jbuilder
+++ app/views/api/v2/product/products/index.json.jbuilder
@@ -4,12 +4,4 @@ json.array! @products do |product|
 	json.company do
 		json.partial! "api/v2/company/companies/attributes", company: product.company
 	end
-	
-	if product.clothes
-		json.clothing_sizes do
-			json.array! product.clothing_sizes do |clothing_size|
-				json.title clothing_size.title
-			end
-		end
-	end
 end
