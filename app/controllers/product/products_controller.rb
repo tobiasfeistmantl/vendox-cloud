73349f26fc8c639c4ff05d8ded872786c344ace6
Tobias Feistmantl <tobias@feistmantl.net>
2015-08-22 20:46:03 +0200

    Fix device parameter bug

--- app/controllers/product/products_controller.rb
+++ app/controllers/product/products_controller.rb
@@ -2,8 +2,8 @@ class Product::ProductsController < Product::Base
 	def index
 		@q = Product.includes(:company).active.ransack(params[:q])
 
-		if params[:location].present? && params[:lng].present? && params[:lat].present?
-			new_position = current_user_session(create_if_nil: true).positions.create(latitude: params[:lat], longitude: params[:lng])
+		if params[:location].present? && params[:lng].present? && params[:lat].present? && current_user_session.present?
+			new_position = current_user_session.positions.create(latitude: params[:lat], longitude: params[:lng])
 			session[:user_address] = params[:location]
 		end
 
