f8e1e1a311af7768be5f09f5960ffd39b25909fe
Tobias Feistmantl <tobias@feistmantl.net>
2015-08-22 22:24:10 +0200

    Change param key name to search

--- app/controllers/api/v2/product/products_controller.rb
+++ app/controllers/api/v2/product/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V2::Product::ProductsController < Api::V2::Product::Base
 	def index
-		@q = ::Product.active.includes(:company).ransack(params[:q])
+		@q = ::Product.active.includes(:company).ransack(name_cont: params[:search])
 
 		if coordinates = @user_session.coordinates
 			@products = @q.result.near(coordinates, 5000).paginate(page: params[:page])
